32ef4d58d97a91e72e3fdbd444fceb0054f494f8
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-10-04 17:25:17 -0400

    Lane movement done

---
+++ Assets/Scripts/LaneMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneMovement : MonoBehaviour 
+{
+    public float sideSpeed;
+    public float sideDisp;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+           
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StartCoroutine(ShiftLane(-1));
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StartCoroutine(ShiftLane(1));
+        }
+	}
+
+    IEnumerator ShiftLane(int moveDirection)
+    {
+        float displacement = Mathf.Clamp(transform.position.x + (moveDirection * sideDisp), -sideDisp, +sideDisp);
+        while (Mathf.Abs(Mathf.Abs(transform.position.x) - Mathf.Abs(displacement)) > 0.0f)
+        {
+            float step = sideSpeed * Time.deltaTime;
+            Vector3 target = new Vector3(displacement, transform.position.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, target , step);
+            yield return null;
+        }
+    }
+}
