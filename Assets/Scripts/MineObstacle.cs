1432eadf6ab102e303a8cfe5b249315bcb26f10d
Srinivasan <tsrivishnu99@github.com>
2015-10-05 14:12:37 -0400

    Sound obstacle completely finished and vibration mine separate class created.

---
+++ Assets/Scripts/MineObstacle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class MineObstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(StartVibration());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        StopVibration();
+    }
+
+    IEnumerator StartVibration()
+    {
+        float increment = 0.0f;
+        while (increment <= 1.0f)
+        {
+            GamePad.SetVibration(0, increment, increment);
+            increment += 0.01f;
+            yield return null;
+        }
+        StopVibration();
+    }
+
+    void StopVibration()
+    {
+        GamePad.SetVibration(0, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/soundObstacle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class soundObstacle : MonoBehaviour {
+
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        //this.gameObject.GetComponent<BoxCollider>().center = new Vector3(10.0f,0.0f,this.gameObject.GetComponent<BoxCollider>().size.z / 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            audioSource.mute = false;
+            Vector3 playerPosition = other.gameObject.transform.position;
+            float pan = this.transform.position.x - playerPosition.x;
+            
+            if (Mathf.Abs(pan) >= 2.0f)
+                pan = pan / Mathf.Abs(pan);
+            else
+                pan = 0;
+
+            audioSource.panStereo = pan;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        audioSource.mute = true;
+        //Destroy(this.gameObject);
+    }
+}
