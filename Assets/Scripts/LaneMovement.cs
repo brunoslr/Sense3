3eed57a6e214d880c4ea998e443c29e5086d8b27
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-10-05 10:57:03 -0400

    Vibration works

--- Assets/Scripts/LaneMovement.cs
+++ Assets/Scripts/LaneMovement.cs
@@ -1,27 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 public class LaneMovement : MonoBehaviour 
 {
     public float sideSpeed;
     public float sideDisp;
     public float forwardspeed;
-	
+
+    public GameObject vibrate;
+
+    private float horizontalAxis;
+
 	// Update is called once per frame
-	void Update () 
+    void Update()
     {
         transform.Translate(transform.forward * forwardspeed * Time.deltaTime);
-           
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+
+        horizontalAxis = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             StartCoroutine(ShiftLane(-1));
         }
 
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             StartCoroutine(ShiftLane(1));
         }
-	}
+
+        if (vibrate.transform.position.z - transform.position.z < 0.0f)
+        {
+            StopVibration();
+        }
+
+        else if(vibrate.transform.position.z - transform.position.z < 10.0f)
+        {
+            StartCoroutine(StartVibration());
+        }
+    }
 
     IEnumerator ShiftLane(int moveDirection)
     {
@@ -33,5 +50,21 @@ public class LaneMovement : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, target , step);
             yield return null;
         }
+    }
+
+    IEnumerator StartVibration()
+    {
+        float increment = 0.0f;
+        while (increment <= 1.0f)
+        {
+            GamePad.SetVibration(0, increment, increment);
+            increment += 0.01f;
+            yield return null;
+        }
+    }
+
+    void StopVibration()
+    {
+        GamePad.SetVibration(0, 0.0f, 0.0f);
     }
 }
