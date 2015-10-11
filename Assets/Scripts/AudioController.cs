cf92a28d66dce0ecd7cd840a91d72ca035bb3879
unknown <sxt5244@GDD-01.main.ad.rit.edu>
2015-10-11 13:38:28 -0400

    Audio architecture changed.

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioClip[] soundtracks;
+    private int counter,maxLevel;
+    private AudioSource[] audioSources;
+
+	// Use this for initialization
+	void Start () {
+        maxLevel = soundtracks.Length;
+        counter = 0;
+
+        for(int i=0; i< maxLevel; i++)
+        {
+            this.gameObject.AddComponent<AudioSource>();
+        }
+        audioSources = this.gameObject.GetComponents<AudioSource>();
+
+        for(int i=0; i< maxLevel; i++)
+        {
+            audioSources[i].clip = soundtracks[i];
+            audioSources[i].mute = true;
+            audioSources[i].panStereo = 0;
+            audioSources[i].volume = 1;
+            audioSources[i].loop = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playPermanent()
+    {
+        //if(other.name.Contains("Sound"))
+        if(counter < maxLevel)
+        {
+            audioSources[counter].panStereo = 0;
+            audioSources[counter].mute = false;
+            audioSources[counter].timeSamples = audioSources[0].timeSamples;
+            audioSources[counter].Play();
+            counter++;
+        }
+    }
+
+    public void playTemp(int pan)
+    {
+        audioSources[counter].panStereo = pan;
+        audioSources[counter].mute = false;
+        audioSources[counter].timeSamples = audioSources[0].timeSamples;
+        audioSources[counter].Play();
+    }
+
+    public void stopTemp()
+    {
+        audioSources[counter].mute = true ;
+    }
+}
--- Assets/Scripts/soundObstacle.cs
+++ Assets/Scripts/soundObstacle.cs
@@ -7,11 +7,12 @@ using System.Collections;
 
 public class soundObstacle : MonoBehaviour {
 
-    private AudioSource[] audioSource;
+    //private AudioSource[] audioSource;
     //private AudioSource CenteraudioSource;
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
-        audioSource = this.gameObject.GetComponents<AudioSource>();
+        //audioSource = this.gameObject.GetComponents<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,33 +20,36 @@ public class soundObstacle : MonoBehaviour {
 	
 	}
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-           
-           
+                       
             Vector3 playerPosition = other.gameObject.transform.position;
             float pan = this.transform.position.x - playerPosition.x;
 
+            //audioSource[0].timeSamples = other.gameObject.GetComponent<AudioSource>().timeSamples;
+            //audioSource[1].timeSamples = other.gameObject.GetComponent<AudioSource>().timeSamples;
+            
             if (Mathf.Abs(pan) >= 2.0f)
             {
                 pan = pan / Mathf.Abs(pan);
-                audioSource[0].panStereo = pan;
-                audioSource[0].volume = Mathf.Abs(pan);
-                audioSource[0].mute = false;
-                audioSource[0].Play();
-              
+                //audioSource[0].panStereo = pan;
+                //audioSource[0].volume = Mathf.Abs(pan);
+                //audioSource[0].mute = false;
+                //audioSource[0].Play();
+            
             }
             else
             {
                 pan = 0;
-                audioSource[1].panStereo = pan;
-                audioSource[1].volume = 1 - Mathf.Abs(pan);
-                audioSource[1].mute = false;
-                audioSource[1].Play();
+                //audioSource[1].panStereo = pan;
+                //audioSource[1].volume = 1 - Mathf.Abs(pan);
+                //audioSource[1].mute = false;
+                //audioSource[1].Play();
             }
 
+            other.gameObject.GetComponentInChildren<AudioController>().playTemp((int) pan);
 
   
         }
@@ -53,9 +57,13 @@ public class soundObstacle : MonoBehaviour {
 
     void OnTriggerExit(Collider other)
     {
-        audioSource[0].loop = false;
-        audioSource[1].loop = false;
-        audioSource[0].mute = true;
-        audioSource[1].mute = true;
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponentInChildren<AudioController>().stopTemp();
+        }
+       // audioSource[0].loop = false;
+       // audioSource[1].loop = false;
+       // audioSource[0].mute = true;
+       // audioSource[1].mute = true;
     }
 }
