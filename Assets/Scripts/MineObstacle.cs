8d987a74e156a612f29bf5f516d220affcfe399a
Lakshminarayanan <lv1018@rit.edu>
2015-10-13 19:22:14 -0400

    GG Commit - Added Equalizer background and Mine Spikes.

--- Assets/Scripts/MineObstacle.cs
+++ Assets/Scripts/MineObstacle.cs
@@ -9,11 +9,16 @@ public class MineObstacle : MonoBehaviour {
 
     private IEnumerator stopVibration;
     private bool breaknow;
+    public GameObject Spikes;
+    Spectrum _spectrum;
 	// Use this for initialization
 	void Start () {
+        _spectrum = GameObject.Find("BGCamera").GetComponent<Spectrum>();
         stopVibration = StartVibration();
         breaknow = false;
         this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        if (this.gameObject.transform.childCount > 2)
+            Spikes = this.gameObject.transform.GetChild(1).gameObject;
 	}
 	
 	// Update is called once per frame
@@ -27,8 +32,25 @@ public class MineObstacle : MonoBehaviour {
         {
             StartCoroutine(StartVibration());
         }
+       
     }
-
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            LetsBounce();
+        }
+    }
+    void LetsBounce()
+    {
+        for(int i =0; i<15; i++){
+            Vector3 prevScale = Spikes.transform.GetChild(i).localScale;
+            prevScale.y = Mathf.Lerp(prevScale.y, _spectrum.spectrum[Random.Range(0, 10)] * 50, Time.deltaTime * 10);
+            Spikes.transform.GetChild(i).localScale = prevScale;
+            Spikes.transform.position = new Vector3(Spikes.transform.position.x, prevScale.y / 2 + 1, Spikes.transform.position.z);
+        }
+       
+    }
     void OnTriggerExit(Collider other)
     {
         //StopCoroutine(stopVibration);
