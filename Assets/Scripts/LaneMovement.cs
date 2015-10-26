fd9eb326b9986fa8c3aff20b9c0314c8c7330f2e
Srinivasan <tsrivishnu99@github.com>
2015-10-26 18:34:53 -0400

    Trail Added.

--- Assets/Scripts/LaneMovement.cs
+++ Assets/Scripts/LaneMovement.cs
@@ -10,6 +10,8 @@ public class LaneMovement : MonoBehaviour
     public float tilt;
     public float jumpDisp;
     public float jumpSpeed;
+    public float trailTime = 2.0f;
+
 
     private uint hitCounter;
     private float baseSpeed;
@@ -35,6 +37,8 @@ public class LaneMovement : MonoBehaviour
         {
             hitCounter += 1;
             forwardspeed = (1.0f + (0.75f * hitCounter)) * baseSpeed;
+            this.gameObject.GetComponent<TrailRenderer>().enabled = true;
+            StartCoroutine(endTrail());
             this.gameObject.GetComponentInChildren<AudioController>().incrementCounter();
         }
     }
@@ -85,5 +89,11 @@ public class LaneMovement : MonoBehaviour
         }
 
         getJumpInput = true;
+    }
+
+    IEnumerator endTrail()
+    {
+        yield return new WaitForSeconds(trailTime);
+        this.gameObject.GetComponent<TrailRenderer>().enabled = false;
     }
 }
