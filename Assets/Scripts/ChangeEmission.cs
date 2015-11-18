495484ac4219cc46fa985e1482471ad0c61d2304
unknown <amb6352@IGMGRAD-14.main.ad.rit.edu>
2015-11-18 15:56:42 -0500

    Obstacles changing color

---
+++ Assets/Scripts/ChangeEmission.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeEmission : MonoBehaviour {
+    private Material material;
+    private float emission;
+    // Use this for initialization
+    void Start () {
+        material = this.GetComponent<MeshRenderer>().material;
+        material.EnableKeyword("_EMISSION");
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        emission = Mathf.Sin(Time.time);
+        material.SetColor("_EmissionColor", (1 - emission) * material.GetColor("_Color"));
+        material.SetColor("_Metallic", emission * material.GetColor("_Color"));
+        material.SetColor("_Glossiness", emission * material.GetColor("_Color"));
+    }
+}
