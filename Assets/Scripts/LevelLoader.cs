6978df356f4e3f00d3ee28153c1b529aa12e9959
Lakshminarayanan Vijayaraghavan <lv1018@rit.edu>
2016-01-30 12:40:54 -0500

    Added Ui

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Button startButton;
+    public Button quitButton;
+	// Use this for initialization
+	void Start () {
+        startButton.onClick.AddListener(() => { LoadGame(); });
+        quitButton.onClick.AddListener(() => { QuitGame(); });
+
+	}
+
+    public void LoadGame()
+    {
+        Application.LoadLevel("test2");
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PauseManager : MonoBehaviour {
+
+    public Button resumeButton;
+    public Button restartButton;
+    public Button mainMenuButton;
+    public Button quitButton;
+
+    public CanvasGroup panel;
+
+    public bool IsPaused { get; private set; }
+    float targetAlpha;
+    public void Start()
+    { 
+        resumeButton.onClick.AddListener(() => { OnResumeButtonClick(); });
+        restartButton.onClick.AddListener(() => { OnRestartButtonClick(); });
+        quitButton.onClick.AddListener(() => { OnQuitButtonClick(); });
+        mainMenuButton.onClick.AddListener(() => { OnMainMenuButtonClick(); });
+    }
+
+    private void OnQuitButtonClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void OnMainMenuButtonClick()
+    {
+        Pause();
+        Application.LoadLevel("MainMenu");
+    }
+
+    private void OnRestartButtonClick()
+    {
+        Pause();
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    private void OnResumeButtonClick()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        IsPaused = !IsPaused;
+        panel.interactable = IsPaused;
+        Time.timeScale = IsPaused ? 0 : 1;
+        targetAlpha = IsPaused ? 1 : 0;
+    }
+
+    private void Update()
+    {
+        panel.alpha = Mathf.Lerp(panel.alpha, targetAlpha, 0.25f);
+        if (Input.GetKeyUp(KeyCode.Escape)) 
+        {
+            Pause();
+        }
+    }
+	
+}
