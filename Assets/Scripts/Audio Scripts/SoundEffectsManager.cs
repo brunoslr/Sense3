f7fa6548d84bc9701b27cab6adbc16279e42fdb6
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2016-01-31 17:43:01 -0500

    Added Sound effects manager. Sounds are in Sounds->SoundEffects folder.

---
+++ Assets/Scripts/Audio Scripts/SoundEffectsManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectsManager : MonoBehaviour {
+
+    public AudioClip visObsCrashSound;
+    public AudioClip minObsCrashSound;
+    public AudioClip audPickupSound;
+    public AudioClip movPlayerSound;
+
+    private AudioSource visObsAudioSource;
+    private AudioSource minObsAudioSource;
+    private AudioSource audPickupAudioSource;
+    private AudioSource movPlayerAudioSource;
+
+	// Use this for initialization
+	void Start () {
+	    visObsAudioSource =  this.gameObject.AddComponent<AudioSource>();
+        minObsAudioSource = this.gameObject.AddComponent<AudioSource>();
+        audPickupAudioSource = this.gameObject.AddComponent<AudioSource>();
+        movPlayerAudioSource = this.gameObject.AddComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void VisualObstacleCrashSound()
+    {
+        visObsAudioSource.PlayOneShot(visObsCrashSound);
+    }
+
+    public void MineObstacleCrashSound()
+    {
+        minObsAudioSource.PlayOneShot(minObsCrashSound);
+    }
+
+    public void AudioObstaclePickupSound()
+    {
+        audPickupAudioSource.PlayOneShot(audPickupSound);
+    }
+
+    // This sound should propogate from left ear to right ear if player moves right and vice versa
+    public void MovePlayerSound()
+    {
+        movPlayerAudioSource.PlayOneShot(movPlayerSound);
+    }
+}
