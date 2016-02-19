efebd1a3dcaf09cf3b307342cf3465bf1c8be6bb
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2016-02-19 18:12:33 -0500

    Jump added but not even.

--- Assets/Scripts/Obstacles scripts/CreateTactileObstacle.cs
+++ Assets/Scripts/Obstacles scripts/CreateTactileObstacle.cs
@@ -11,7 +11,7 @@ public class CreateTactileObstacle : MonoBehaviour {
 	void Start () {
         transform.localScale = scale;
         rumblePickup = transform.GetChild(0);
-        rumblePickup.localScale = new Vector3(transform.localScale.x / transform.localScale.x, transform.localScale.y / transform.localScale.y, transform.localScale.z / transform.localScale.z / 100.0f);
+        rumblePickup.localScale = new Vector3(1.0f, 1.0f, 1.0f / 100.0f);
         rumblePickup.localPosition = new Vector3(0.0f, 0.0f, (transform.localScale.z / 2.0f / transform.localScale.z) - (rumblePickup.localScale.z * 5.0f / 10.0f));
     }
 	
