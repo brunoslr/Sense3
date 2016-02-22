53e9b9606d3076a62ad9f893ea92267389d623f3
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2016-02-22 12:57:44 -0500

    Small changes.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -58,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
 
     //private LineRenderer lineRenderer;
 
-    public bool playerInsideMine;
+    [HideInInspector]public bool playerInsideMine;
 
     void Start()
     {
