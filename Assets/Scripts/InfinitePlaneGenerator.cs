5121fe89cc657dcf31918be35bb88c10c7120a9c
Mrinal Jain <mnj8396@rit.edu>
2016-02-23 18:17:51 -0500

    #64: Moved terrain stuff to plane.

--- Assets/Scripts/InfinitePlaneGenerator.cs
+++ Assets/Scripts/InfinitePlaneGenerator.cs
@@ -1,28 +1,83 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class InfinitePlaneGenerator : MonoBehaviour
 {
 
-    public GameObject plane; // The water plane object
-    public GameObject player; // The first-person object
+    public GameObject planePrefab; 
+    public GameObject player;
 
+    //Visual obstacle requirements
+    public GameObject[] visualObstaclePrefabs;
+    public float xOffsetVisual;
+    public float yOffsetVisual;
+    public float zOffsetVisual;
+    public int xClamp;
+
+    //Sound obstacle placement requirements
+    public GameObject soundObstaclePrefab;
+    public float xOffsetSound;
+    public float yOffsetSound;
+    public float zOffsetSound;
+    public float zOffsetFromPlayerSound;
+
+    //Tactile obstacle placement requirements
+    public GameObject tactileObstaclePrefab;
+    public float xOffsetTactile;
+    public float yOffsetTactile;
+    public float zOffsetTactile;
+    public float zOffsetFromPlayerTactile;
+
+    //Plane update requirements
     private float sizeOfPlaneX;
     private float sizeOfPlaneZ;
-
     private GameObject[,] planes = new GameObject[3, 3];
 
-    // Use this for initialization
+    private int playerXPosition;
+    //private float playerYPosition; removed -- Unused
+    private int playerZPosition;
+
+    private List<GameObject> visualObstacles;
+    private List<GameObject> loadedVisualObstacles;
+    private GameObject visualObstacle;
+
+    private int zPosVisualObstacle;
+    private int xPosVisualObstacle;
+
+    private GameObject soundObstacle;
+    private float zOffsetBetweenSoundObstacle;
+
+    private GameObject tactileObstacle;
+    private float zOffsetBetweenTactileObstacle;
+
+    private int lowerClamp;
+    private int upperClamp;
+
+    private int nextZ;
+
     void Start()
     {
-        sizeOfPlaneX = plane.GetComponent<Renderer>().bounds.size.x;
-        sizeOfPlaneZ = plane.GetComponent<Renderer>().bounds.size.z;
+        SetupPlaneGrid();
+
+        LoadVisualObstacles();
+
+        LoadSoundObstacle();
+
+        LoadTactileObstacle();
+
+        InitializeVisualObstacles();
+    }
+
+    private void SetupPlaneGrid()
+    {
+        sizeOfPlaneX = planePrefab.GetComponent<Renderer>().bounds.size.x;
+        sizeOfPlaneZ = planePrefab.GetComponent<Renderer>().bounds.size.z;
 
         for (int i = 0; i < 3; i++)
         {
             for (int c = 0; c < 3; c++)
             {
-                planes[i, c] = GameObject.Instantiate(plane) as GameObject;
+                planes[i, c] = GameObject.Instantiate(planePrefab) as GameObject;
                 planes[i, c].name = i + "," + c;
             }
         }
@@ -37,14 +92,87 @@ public class InfinitePlaneGenerator : MonoBehaviour
                 planes[i, c].transform.position = tempPos;
             }
         }
-
     }
 
-    // Update is called once per frame
+    private void LoadVisualObstacles()
+    {
+        lowerClamp = (int)player.transform.position.x - xClamp;
+        upperClamp = (int)player.transform.position.x + xClamp;
+
+        xPosVisualObstacle = (int)player.transform.position.x;
+
+        visualObstacles = new List<GameObject>();
+        loadedVisualObstacles = new List<GameObject>();
+
+        GameObject temp;
+        for (int i = 0; i < visualObstaclePrefabs.Length * 20; i++)
+        {
+            temp = Instantiate(visualObstaclePrefabs[i % visualObstaclePrefabs.Length]);
+            temp.SetActive(false);
+            loadedVisualObstacles.Add(temp);
+        }
+
+        zPosVisualObstacle = (int)zOffsetVisual;
+        nextZ = (int)(zOffsetVisual - (zOffsetVisual / 2));
+    }
+
+    private void LoadSoundObstacle()
+    {
+        zOffsetBetweenSoundObstacle = zOffsetSound;
+        soundObstacle = Instantiate(soundObstaclePrefab);
+        soundObstacle.SetActive(false);
+    }
+
+    private void LoadTactileObstacle()
+    {
+        zOffsetBetweenTactileObstacle = zOffsetTactile;
+        tactileObstacle = Instantiate(tactileObstaclePrefab);
+        tactileObstacle.SetActive(false);
+    }
+
+    private void InitializeVisualObstacles()
+    {
+        int pick;
+        float xpos = xPosVisualObstacle - (2 * xOffsetVisual);
+        float zpos = zPosVisualObstacle;
+        for (int i = 0; i < 5; ++i)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                pick = Random.Range(0, loadedVisualObstacles.Count - 1);
+                visualObstacle = loadedVisualObstacles[pick];
+                visualObstacle.SetActive(true);
+                loadedVisualObstacles.RemoveAt(pick);
+                visualObstacles.Add(visualObstacle);
+                visualObstacle.transform.position = new Vector3(xpos, yOffsetVisual, zpos);
+                xpos += xOffsetVisual;
+
+            }
+            zpos += zOffsetVisual;
+            xpos = xPosVisualObstacle - (2 * xOffsetVisual);
+        }
+        zPosVisualObstacle = (int)(zpos - zOffsetVisual);
+    }
+
     void Update()
     {
-        var playerX = player.transform.position.x;
-        var playerZ = player.transform.position.z;
+        playerXPosition = (int)player.transform.position.x;
+        //playerYPosition = player.transform.position.y; // Removed, not used
+        playerZPosition = (int)player.transform.position.z;
+
+        UpdatePlane();
+
+        UpdateVisualObstacles();
+
+        DeleteVisualObstacles();
+
+        UpdateSoundObstacle();
+
+        UpdateTactileObstacle();
+    }
+
+    private void UpdatePlane()
+    {
         for (int i = 0; i < 3; i++)
         {
             for (int c = 0; c < 3; c++)
@@ -53,34 +181,31 @@ public class InfinitePlaneGenerator : MonoBehaviour
                 float planeXMax = planeXMin + sizeOfPlaneX;
                 float planeZMin = planes[i, c].transform.position.z - (sizeOfPlaneZ / 2);
                 float planeZMax = planeZMin + sizeOfPlaneZ;
-                if (playerX > planeXMin && playerX < planeXMax && playerZ > planeZMin && playerZ < planeZMax)
+                if (playerXPosition > planeXMin && playerXPosition < planeXMax && playerZPosition > planeZMin && playerZPosition < planeZMax)
                 {
-                    updatePlanes(i, c);
+                    UpdatePlaneGrid(i, c);
                 }
             }
         }
     }
 
-    public void updatePlanes(int x, int z)
+    private void UpdatePlaneGrid(int x, int z)
     {
         if (x == 0)
         {
-            // Gone left;
-            moveLeft();
+            MoveLeft();
         }
         else if (x == 2)
         {
-            // Gone right;
-            moveRight();
+            MoveRight();
         }
         if (z == 0)
         {
-            // Gone up;
-            moveUp();
+            MoveAhead();
         }
     }
 
-    public void moveLeft()
+    private void MoveLeft()
     {
         GameObject[,] newPlanes = new GameObject[3, 3];
         for (int i = 0; i < 3; i++)
@@ -99,7 +224,7 @@ public class InfinitePlaneGenerator : MonoBehaviour
         planes = newPlanes;
     }
 
-    public void moveRight()
+    private void MoveRight()
     {
         GameObject[,] newPlanes = new GameObject[3, 3];
         for (int i = 0; i < 3; i++)
@@ -118,7 +243,7 @@ public class InfinitePlaneGenerator : MonoBehaviour
         planes = newPlanes;
     }
 
-    public void moveUp()
+    private void MoveAhead()
     {
         GameObject[,] newPlanes = new GameObject[3, 3];
         for (int i = 0; i < 3; i++)
@@ -135,5 +260,113 @@ public class InfinitePlaneGenerator : MonoBehaviour
             newPlanes[2, i] = planes[2, c];
         }
         planes = newPlanes;
+    }
+
+    private void UpdateVisualObstacles()
+    {
+        if ((int)playerXPosition < lowerClamp)
+        {
+            xPosVisualObstacle -= (int)xOffsetVisual;
+            upperClamp = lowerClamp;
+            lowerClamp -= 2 * xClamp;
+            CreateNewPuzzleOnSide(-1.0f);
+        }
+
+        if ((int)playerXPosition > upperClamp)
+        {
+            xPosVisualObstacle += (int)xOffsetVisual;
+            lowerClamp = upperClamp;
+            upperClamp += 2 * xClamp;
+            CreateNewPuzzleOnSide(1.0f);
+        }
+
+        if (playerZPosition > nextZ)
+        {
+            nextZ += (int)zOffsetVisual;
+            zPosVisualObstacle += (int)zOffsetVisual;
+            GenerateVisualObstacles();
+        }
+    }
+
+    private void GenerateVisualObstacles()
+    {
+        int pick;
+        float xpos = xPosVisualObstacle - (2 * xOffsetVisual);
+        for (int i = 0; i < 5; ++i)
+        {
+            pick = Random.Range(0, loadedVisualObstacles.Count);
+            visualObstacle = loadedVisualObstacles[pick];
+            visualObstacle.SetActive(true);
+            loadedVisualObstacles.RemoveAt(pick);
+            visualObstacles.Add(visualObstacle);
+            visualObstacle.transform.position = new Vector3(xpos, yOffsetVisual, zPosVisualObstacle);
+            xpos += xOffsetVisual;
+        }
+    }
+
+    private void CreateNewPuzzleOnSide(float dir)
+    {
+        float xpos = xPosVisualObstacle + (2 * xOffsetVisual * dir);
+        for (int i = -5; i < 1; i++)
+        {
+            Vector3 newPosition = new Vector3(xpos, yOffsetVisual, zPosVisualObstacle + (zOffsetVisual * i));
+            if ((Physics.OverlapBox(newPosition, Vector3.one * ((xOffsetVisual / 2.0f) - 20.0f))).Length == 0)
+            {
+                int pick = Random.Range(0, loadedVisualObstacles.Count);
+                GameObject visualObstacle = loadedVisualObstacles[pick];
+                visualObstacle.SetActive(true);
+                loadedVisualObstacles.RemoveAt(pick);
+                visualObstacles.Add(visualObstacle);
+
+                visualObstacle.transform.position = new Vector3(xpos, yOffsetVisual, zPosVisualObstacle + (zOffsetVisual * i));
+            }
+        }
+    }
+
+    private void DeleteVisualObstacles()
+    {
+        Transform visualObstacle;
+        for (int i = 0; i < visualObstacles.Count; i++)
+        {
+            visualObstacle = visualObstacles[i].transform;
+            if (visualObstacle.position.z + 150.0f < playerZPosition)
+            {
+                visualObstacle.Rotate(Vector3.zero);
+                visualObstacle.gameObject.SetActive(false);
+                visualObstacles.Remove(visualObstacle.gameObject);
+                loadedVisualObstacles.Add(visualObstacle.gameObject);
+            }
+        }
+    }
+
+    private void UpdateSoundObstacle()
+    {
+        if (playerZPosition > zOffsetSound)
+        {
+            zOffsetSound += zOffsetBetweenSoundObstacle;
+            GenerateSoundObstacle(playerZPosition + zOffsetFromPlayerSound + soundObstacle.transform.localScale.z);
+        }
+    }
+
+    private void GenerateSoundObstacle(float zPosition)
+    {
+        soundObstacle.SetActive(true);
+        soundObstacle.GetComponentInChildren<PickUpScript>().pickedUp = false;
+        soundObstacle.transform.position = new Vector3(Random.Range(lowerClamp, upperClamp), yOffsetSound, zPosition);
+    }
+
+    private void UpdateTactileObstacle()
+    {
+        if (playerZPosition > zOffsetTactile)
+        {
+            zOffsetTactile += zOffsetBetweenTactileObstacle;
+            GenerateTactileObstacle(playerZPosition + zOffsetFromPlayerTactile + tactileObstacle.transform.localScale.z);
+        }
+    }
+
+    private void GenerateTactileObstacle(float zPosition)
+    {
+        tactileObstacle.SetActive(true);
+        tactileObstacle.transform.position = new Vector3(Random.Range(lowerClamp, upperClamp), yOffsetTactile, zPosition);
     }
 }
