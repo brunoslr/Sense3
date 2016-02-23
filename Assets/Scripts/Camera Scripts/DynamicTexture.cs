ad7799bf0a0e045ace964b089619007a8a0544f0
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2016-02-23 11:27:33 -0500

    testing for new fractal placement.

--- Assets/Scripts/Camera Scripts/DynamicTexture.cs
+++ Assets/Scripts/Camera Scripts/DynamicTexture.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class DynamicTexture : MonoBehaviour 
 {
     public ComputeShader computeShader;
-    public Material skybox;
+    //public Material skybox;
     public Texture2D palette;
 
     private RenderTexture tex;
@@ -18,13 +18,14 @@ public class DynamicTexture : MonoBehaviour
         tex = new RenderTexture(512, 512, 24);
         tex.enableRandomWrite = true;
         tex.Create();
-        RenderSettings.skybox = skybox;
-        skybox.SetTexture("_FrontTex", tex);
-        skybox.SetTexture("_BackTex", tex);
-        skybox.SetTexture("_TopTex", tex);
-        skybox.SetTexture("_BottomTex", tex);
-        skybox.SetTexture("_LeftTex", tex);
-        skybox.SetTexture("_RightTex", tex);
+        //RenderSettings.skybox = skybox;
+        //skybox.SetTexture("_FrontTex", tex);
+        //skybox.SetTexture("_BackTex", tex);
+        //skybox.SetTexture("_TopTex", tex);
+        //skybox.SetTexture("_BottomTex", tex);
+        //skybox.SetTexture("_LeftTex", tex);
+        //skybox.SetTexture("_RightTex", tex);
+        this.GetComponent<MeshRenderer>().material.mainTexture = tex;
 
         computeShader.SetTexture(kernelHandle, "palette", palette);
    
