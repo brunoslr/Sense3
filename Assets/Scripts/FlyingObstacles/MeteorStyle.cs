a995a221ea7fd2fb84f15a508457f682a1dd11d2
Mrinal Jain <mnj8396@rit.edu>
2016-02-27 17:59:59 -0500

    Added dynamic visual obstacles. Added level progression to tactile obstacles.

---
+++ Assets/Scripts/FlyingObstacles/MeteorStyle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace DynamicObstacles
+{
+    public class MeteorStyle : MonoBehaviour
+    {
+        public Material particleMaterial;
+
+        void OnEnable()
+        {
+            var playerPosition = GameObject.Find("Player").transform.position;
+            transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 1000);
+        }
+    }
+}
