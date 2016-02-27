bcc19b15c173dbf95e81a5b9cccda7fdea29c689
Lakshminarayanan Vijayaraghavan <lv1018@rit.edu>
2016-02-27 18:06:12 -0500

    Music Glow - De activated Line rendered and enabled collision with music

--- Assets/Scripts/CoreSystem.cs
+++ Assets/Scripts/CoreSystem.cs
@@ -31,7 +31,7 @@ public class CoreSystem : MonoBehaviour {
     {
         coolDownTimeInSeconds = coolDownTime;
         instance = this;
-        coolDownFlag = true; 
+        coolDownFlag = false; 
     }
 
     // Trigger Function for OnCollisionSoundEvents -- Function is triggered when you pick up the sound
