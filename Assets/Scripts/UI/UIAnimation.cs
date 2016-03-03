6fa3c7ddf51189087dc18d2c2a87f0f69bfdc38a
Lakshminarayanan Vijayaraghavan <lv1018@rit.edu>
2016-03-03 17:12:51 -0500

    Menu works both ways. Press A or D

--- Assets/Scripts/UI/UIAnimation.cs
+++ Assets/Scripts/UI/UIAnimation.cs
@@ -21,22 +21,21 @@ public class UIAnimation : MonoBehaviour {
 	void Update () {
         time += Time.deltaTime;
 
-        //player.transform.Rotate(player.transform.up, speed * Time.deltaTime);
-
        if(Input.GetKeyDown(KeyCode.A))
         {
            
-            StartCoroutine("MoveUI");
+            StartCoroutine("MoveUILeft");
             isRunning = true;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
 
-
+            StartCoroutine("MoveUIRight");
+            isRunning = true;
         }
     }
 
-    IEnumerator MoveUI()
+    IEnumerator MoveUILeft()
     {
         if (!isRunning)
         {
@@ -47,8 +46,6 @@ public class UIAnimation : MonoBehaviour {
             positions[2] = menuElements[2].transform.position;
             positions[3] = menuElements[3].transform.position;
 
-
-            //while (Vector3.Distance(menuElements[0].transform.position, menuElements[1].transform.position) != 0)
             while (Vector3.Distance(menuElements[0].transform.position, positions[1]) > 0.1)
             {
                 for (int i = 0; i < 4; i++)
@@ -57,6 +54,29 @@ public class UIAnimation : MonoBehaviour {
                 }
 
                 yield return null;  
+            }
+            isRunning = false;
+        }
+    }
+    IEnumerator MoveUIRight()
+    {
+        if (!isRunning)
+        {
+
+            time = 0;
+            positions[0] = menuElements[0].transform.position;
+            positions[1] = menuElements[1].transform.position;
+            positions[2] = menuElements[2].transform.position;
+            positions[3] = menuElements[3].transform.position;
+
+            while (Vector3.Distance(menuElements[1].transform.position, positions[0]) > 0.1)
+            {
+                for (int i = 4; i >0; i--)
+                {
+                    menuElements[i % 4].transform.position = Vector3.Lerp(menuElements[i % 4].transform.position, positions[(i - 1) % 4], 10 * time * Time.deltaTime);
+                }
+
+                yield return null;
             }
             isRunning = false;
         }
