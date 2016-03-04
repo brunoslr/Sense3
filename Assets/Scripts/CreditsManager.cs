9fb50d901d188d511984d3495f385855cde9b60c
Mrinal Jain <mnj8396@rit.edu>
2016-03-04 14:25:17 -0500

    Started work on credits scene.

---
+++ Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreditsManager : MonoBehaviour {
+    public GameObject creditsLeft;
+    public GameObject creditsRight;
+    public Text creditsLeftText;
+    public Text creditsRightText;
+
+    private float targetAlphaLeft;
+    private float targetAlphaRight;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
