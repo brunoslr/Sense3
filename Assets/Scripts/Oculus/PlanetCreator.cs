d3f11bd31422d53dd59678d3eec1a91bd71376f4
Srinivasan <tsrivishnu99@github.com>
2016-03-04 20:38:54 -0500

    flow mode commit 2

--- Assets/Scripts/Oculus/PlanetCreator.cs
+++ Assets/Scripts/Oculus/PlanetCreator.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PlanetCreator : MonoBehaviour {
 
     public GameObject[] planetPrefabs;
     public float YDisp = 1000;
     public float minDisp = 10000;
+    public float maxView = 25000;
 
-    private GameObject[] planets;
+    private List<GameObject> planets;
     private GameObject Player;
     private Vector3 previousPos;
-    private int closestPrefabIndex;
     private int totalPrefabs;
+    private List<GameObject> objects_in_front;
+    private int visibility;
 
 	// Use this for initialization
 	void Start () {
         totalPrefabs = planetPrefabs.Length;
         Player = GameObject.FindGameObjectWithTag("Player");
-        planets = new GameObject[totalPrefabs];
+        previousPos = Player.transform.position;
+        planets = new List<GameObject>();
+        objects_in_front = new List<GameObject>();
+        visibility = (int)maxView / (int)minDisp;
         initPlanets();
-        previousPos = Player.transform.position + new Vector3(minDisp, 0, 0);
-        LateUpdate();
 	}
 
     /// <summary>
@@ -28,35 +31,36 @@ public class PlanetCreator : MonoBehaviour {
     /// </summary>
     void initPlanets()
     {
-        Debug.Log(totalPrefabs);
-        for(int i=0;i < totalPrefabs; i++)
-        {
-            planets[i] = (GameObject)Instantiate(planetPrefabs[i], new Vector3(0, 0, - 1000000), new Quaternion(0,0,0,1));
-        }
-        //planets[10].transform.position = Player.transform.position + new Vector3(Random.Range(1.0f, 1.0f) * 1000, YDisp * (Random.Range(-1.0f, 1.0f)), minDisp);
-        closestPrefabIndex = 99;
-        //planets[1].transform.position = /*Player.transform.position + */new Vector3(0, 0, 0);
-        //planets[2].transform.position = /*Player.transform.position + */new Vector3(0, 0, 0);
+       for (int i = 0; i < totalPrefabs; i++)
+       {
+           planets.Add((GameObject)Instantiate(planetPrefabs[i], new Vector3(0, 0, -1000000), new Quaternion(0, 0, 0, 1)));
+       }
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    
+	    if(objects_in_front.Count > 0)
+        {
+            Vector3 playerToPlanet = objects_in_front[0].transform.position - Player.transform.position;
+            float dot = Vector3.Dot(playerToPlanet, Player.transform.position);
+            if(dot < 0)
+            {
+                planets.Add(objects_in_front[0]);
+                objects_in_front.RemoveAt(0);
+            }
+        }
 	}
 
     void LateUpdate()
     {
-        if(Vector3.Distance(Player.transform.position, previousPos) > minDisp)
+        while(objects_in_front.Count < visibility && planets.Count > 0)
         {
-            previousPos = Player.transform.position;
             int index;
-            do
-            {
-                index = Random.Range(0, totalPrefabs);
-            } while (index == closestPrefabIndex);
-            closestPrefabIndex = index;
-            planets[index].transform.position = Player.transform.position + new Vector3(Random.Range(-1.0f,1.0f) * 5000,  YDisp * (Random.Range(-1.0f,1.0f)), minDisp);
-            planets[index].transform.rotation = new Quaternion(Random.Range(-45, 45), Random.Range(-45, 45), Random.Range(-45, 45), 1.0f);
+            index = Random.Range(0, planets.Count - 1);
+            objects_in_front.Add(planets[index]);
+            previousPos = previousPos + new Vector3(Random.Range(-1.0f, 1.0f) * 5000, YDisp * (Random.Range(-1.0f, 1.0f)), minDisp);
+            objects_in_front[objects_in_front.Count - 1].transform.position = previousPos;
+            objects_in_front[objects_in_front.Count - 1].transform.rotation = new Quaternion(Random.Range(-45, 45), Random.Range(-45, 45), Random.Range(-45, 45), 1.0f);
         }
     }
 }
