dc53c6c7a96ee9c9797b97fd5fa80ed5420a558f
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2016-03-06 18:37:16 -0500

    Tutorial scene done

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour {
 
@@ -25,9 +26,11 @@ public class TutorialScript : MonoBehaviour {
 
     public string alignedWithRumble;
     public string notAlignedWithRumble;
+    public string collidedRumble;
     public string passedRumble;
 
     public string passedVisual;
+    public string collidedVisual;
     public string avoidVisual;
 
     public float soundObstacleDispX;
@@ -53,6 +56,13 @@ public class TutorialScript : MonoBehaviour {
     private SoundState leftSoundState;
     private SoundState rightSoundState;
 
+    private bool visualCollided;
+   
+    //Plane update requirements
+    public GameObject planePrefab; 
+    private float sizeOfPlaneX;
+    private float sizeOfPlaneZ;
+    private GameObject[,] planes = new GameObject[3, 3];
 
     void Awake()
     {
@@ -83,11 +93,18 @@ public class TutorialScript : MonoBehaviour {
         rightSoundState = SoundState.BEFORE;
 
         updateState = UpdateState.LEFTSOUND;
-	}
+
+        visualCollided = false;
+
+        CoreSystem.onObstacleEvent += CheckVisualCollision;
+
+        SetupPlaneGrid();
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
+        UpdatePlane();
         switch(updateState)
         {
             case UpdateState.LEFTSOUND:
@@ -182,15 +199,22 @@ public class TutorialScript : MonoBehaviour {
         visualObstacle.SetActive(true);
     }
 
+    void CheckVisualCollision()
+    {
+        screenText.text = collidedVisual;
+        visualCollided = true;
+    }
+
     void VisualUpdate()
     {
-        if (playerGameObject.transform.position.z > visualObstacle.transform.position.z)
+        if (playerGameObject.transform.position.z >= visualObstacle.transform.position.z)
         {
-            screenText.text = passedVisual;
-            // Invike infinite scene after 2 seconds
+            if(!visualCollided)
+                screenText.text = passedVisual;
+            Invoke("SwitchToRumble", nextMechanicTimer);
         }
 
-        else
+        else if(!visualCollided)
         {
             screenText.text = avoidVisual;
         }
@@ -205,15 +229,18 @@ public class TutorialScript : MonoBehaviour {
 
     void RumbleUpdate()
     {
-        if (playerGameObject.transform.position.z > rumblePickup.position.z)
+        if (playerGameObject.transform.position.z >= rumblePickup.position.z)
         {
-            screenText.text = passedRumble;
-            Invoke("SwitchToRumble", nextMechanicTimer);
+            if(Mathf.Abs(playerGameObject.transform.position.x - rumblePickup.position.x) >= rumblePickup.lossyScale.x / 2.0f)
+                screenText.text = passedRumble;
+            else
+                screenText.text = collidedRumble;
+
+            Invoke("CallNextScene", nextMechanicTimer);
         }
 
         else
         {
-
             if (Mathf.Abs(rumbleObstacle.transform.position.x - playerGameObject.transform.position.x) >= rumbleObstacle.transform.localScale.x / 2.0f)
             {
                 screenText.text = notAlignedWithRumble;
@@ -224,5 +251,120 @@ public class TutorialScript : MonoBehaviour {
                 screenText.text = alignedWithRumble;
             }
         }
+    }
+
+    void CallNextScene()
+    {
+        SceneManager.LoadScene("Infinite"); 
+    }
+
+    // plane
+
+    private void SetupPlaneGrid()
+    {
+        sizeOfPlaneX = planePrefab.GetComponent<Renderer>().bounds.size.x;
+        sizeOfPlaneZ = planePrefab.GetComponent<Renderer>().bounds.size.z;
+        Vector3 planePosition;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                planes[i, c] = Instantiate(planePrefab);
+                planes[i, c].name = i + "," + c;
+                planePosition = new Vector3((float)sizeOfPlaneX * (i - 1), -1, (float)sizeOfPlaneZ * ((c - 1) * -1));
+                planes[i, c].transform.position = planePosition;
+            }
+        }
+    }
+
+    private void UpdatePlane()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                float planeXMin = planes[i, c].transform.position.x - (sizeOfPlaneX / 2);
+                float planeXMax = planeXMin + sizeOfPlaneX;
+                float planeZMin = planes[i, c].transform.position.z - (sizeOfPlaneZ / 2);
+                float planeZMax = planeZMin + sizeOfPlaneZ;
+                if (playerGameObject.transform.position.x > planeXMin && playerGameObject.transform.position.x < planeXMax && playerGameObject.transform.position.z > planeZMin && playerGameObject.transform.position.z < planeZMax)
+                {
+                    UpdatePlaneGrid(i, c);
+                }
+            }
+        }
+    }
+
+    private void UpdatePlaneGrid(int x, int z)
+    {
+        if (x == 0)
+        {
+            MoveLeft();
+        }
+        else if (x == 2)
+        {
+            MoveRight();
+        }
+        if (z == 0)
+        {
+            MoveAhead();
+        }
+    }
+
+    private void MoveLeft()
+    {
+        GameObject[,] newPlanes = new GameObject[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 addPos = new Vector3(-sizeOfPlaneX * 3, 0, 0);
+            planes[2, i].transform.position += addPos;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            int c = i - 1;
+            c = (c == -1) ? 2 : c;
+            newPlanes[i, 0] = planes[c, 0];
+            newPlanes[i, 1] = planes[c, 1];
+            newPlanes[i, 2] = planes[c, 2];
+        }
+        planes = newPlanes;
+    }
+
+    private void MoveRight()
+    {
+        GameObject[,] newPlanes = new GameObject[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 addPos = new Vector3(sizeOfPlaneX * 3, 0, 0);
+            planes[0, i].transform.position += addPos;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            int c = i + 1;
+            c = (c == 3) ? 0 : c;
+            newPlanes[i, 0] = planes[c, 0];
+            newPlanes[i, 1] = planes[c, 1];
+            newPlanes[i, 2] = planes[c, 2];
+        }
+        planes = newPlanes;
+    }
+
+    private void MoveAhead()
+    {
+        GameObject[,] newPlanes = new GameObject[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 addPos = new Vector3(0, 0, sizeOfPlaneZ * 3);
+            planes[i, 2].transform.position += addPos;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            int c = i - 1;
+            c = (c == -1) ? 2 : c;
+            newPlanes[0, i] = planes[0, c];
+            newPlanes[1, i] = planes[1, c];
+            newPlanes[2, i] = planes[2, c];
+        }
+        planes = newPlanes;
     }
 }
