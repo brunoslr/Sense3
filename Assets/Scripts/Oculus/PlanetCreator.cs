2b5f2747c93e165937ae20a9f958e29d5f73fd31
Srinivasan <tsrivishnu99@github.com>
2016-03-05 21:40:03 -0500

    Flow mode commit 3

--- Assets/Scripts/Oculus/PlanetCreator.cs
+++ Assets/Scripts/Oculus/PlanetCreator.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 
 public class PlanetCreator : MonoBehaviour {
 
-    public GameObject[] planetPrefabs;
-    public float YDisp = 1000;
-    public float minDisp = 10000;
-    public float maxView = 25000;
+    public GameObject[] Prefabs;
+    public float YDisp = 10000;
+    public int minDisp = 10000;
 
-    private List<GameObject> planets;
+    private List<GameObject> galaxies;
     private GameObject Player;
-    private Vector3 previousPos;
+    private int Counter;
     private int totalPrefabs;
     private List<GameObject> objects_in_front;
-    private int visibility;
+    private int sign;
 
 	// Use this for initialization
 	void Start () {
-        totalPrefabs = planetPrefabs.Length;
+        totalPrefabs = Prefabs.Length;
         Player = GameObject.FindGameObjectWithTag("Player");
-        previousPos = Player.transform.position;
-        planets = new List<GameObject>();
+        Counter = (int) Player.transform.position.z - minDisp/2;
+        galaxies = new List<GameObject>();
         objects_in_front = new List<GameObject>();
-        visibility = (int)maxView / (int)minDisp;
         initPlanets();
+        sign = 1;
 	}
 
     /// <summary>
@@ -31,36 +30,57 @@ public class PlanetCreator : MonoBehaviour {
     /// </summary>
     void initPlanets()
     {
+        GameObject temp;
        for (int i = 0; i < totalPrefabs; i++)
        {
-           planets.Add((GameObject)Instantiate(planetPrefabs[i], new Vector3(0, 0, -1000000), new Quaternion(0, 0, 0, 1)));
+            temp = (GameObject)Instantiate(Prefabs[i]);
+            temp.SetActive(false);
+            galaxies.Add(temp);
        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    if(objects_in_front.Count > 0)
-        {
-            Vector3 playerToPlanet = objects_in_front[0].transform.position - Player.transform.position;
-            float dot = Vector3.Dot(playerToPlanet, Player.transform.position);
-            if(dot < 0)
-            {
-                planets.Add(objects_in_front[0]);
-                objects_in_front.RemoveAt(0);
-            }
-        }
+
 	}
 
     void LateUpdate()
     {
-        while(objects_in_front.Count < visibility && planets.Count > 0)
+        if(Counter + (minDisp/2.0f) <= Player.transform.position.z)
         {
-            int index;
-            index = Random.Range(0, planets.Count - 1);
-            objects_in_front.Add(planets[index]);
-            previousPos = previousPos + new Vector3(Random.Range(-1.0f, 1.0f) * 5000, YDisp * (Random.Range(-1.0f, 1.0f)), minDisp);
-            objects_in_front[objects_in_front.Count - 1].transform.position = previousPos;
-            objects_in_front[objects_in_front.Count - 1].transform.rotation = new Quaternion(Random.Range(-45, 45), Random.Range(-45, 45), Random.Range(-45, 45), 1.0f);
+            float x, y, z;
+            int tempSign = Random.Range(-1, 1);
+            tempSign = tempSign / Mathf.Abs(tempSign);
+
+            x = Random.Range(1, 10) * sign;
+            y = Random.Range(1, 10) * tempSign;
+            z = 1.0f;
+
+            Vector3 dir = new Vector3(x, y, z);
+            dir = Vector3.Normalize(dir);
+            //dir *= minDisp/2.0f;
+            Debug.Log(dir);
+            int index = Random.Range(0, galaxies.Count - 1);
+            galaxies[index].transform.position = Player.transform.position + dir;
+            galaxies[index].transform.forward = Vector3.Normalize(Player.transform.position - galaxies[index].transform.position);
+            galaxies[index].SetActive(true);
+
+            Vector3 dir2 = new Vector3(-x, y * tempSign, z);
+            dir2 = Vector3.Normalize(dir2);
+            Debug.Log(dir2);
+            int index2;
+            do
+            {
+                index2 = Random.Range(0, galaxies.Count - 1);
+            } while (index2 == index);
+
+            galaxies[index2].transform.position = Player.transform.position + dir2;
+            galaxies[index2].transform.forward = Vector3.Normalize(Player.transform.position - galaxies[index2].transform.position);
+            galaxies[index2].transform.Rotate(new Vector3(0, 0, 1), 180.0f);
+            galaxies[index2].SetActive(true);
+
+            sign *= (-1);
+            Counter += minDisp;
         }
     }
 }
