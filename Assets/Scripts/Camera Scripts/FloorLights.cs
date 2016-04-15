4d2366b226a42d6d4e195cadb4e2afef15e2a636
Srinivasan <tsrivishnu99@github.com>
2016-04-15 19:11:52 -0400

    Floor Lights Commit 2: Still incomplete But playable feature is available.

--- Assets/Scripts/Camera Scripts/FloorLights.cs
+++ Assets/Scripts/Camera Scripts/FloorLights.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 #pragma warning disable 618 // Disable obsolete warning for spectrum class
 
@@ -11,9 +12,15 @@ public class FloorLights : MonoBehaviour {
     public GameObject plane;
     public GameObject[] visualizer;
     public uint length = 256;
+    public uint accuracy = 20;
+    public uint fftPrecision = 5;
+    public Transform NewSP_handle;
 
+    private Transform Player;
+    private const uint fullLength = 1024; 
     private float planeLength;
     private float[] spectrum;
+    private float[] spectrumVal;
     private float zscale;
 
     // Use this for initialization
@@ -27,29 +34,63 @@ public class FloorLights : MonoBehaviour {
         length |= length >> 16;
         length++;
 
-        spectrum = new float[length];
+        fftPrecision = (uint) (length ^ ((length ^ fftPrecision) & - (Convert.ToInt32(fftPrecision < length))));
+
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        spectrum = new float[fullLength];
+        spectrumVal = new float[length];
         visualizer = new GameObject[length];
         planeLength = plane.transform.localScale.z;
-        Renderer renderer;
-        Vector3 displacement = new Vector3(0, 0, (length / 2) * 0.03f);
+        //NewSP_handle = gameObject.transform.GetChild(0);
+        //Renderer renderer;
+        Vector3 displacement = new Vector3(0, 0, 0);
         for(int i=0;i < length; i++)
         {
-            visualizer[i] = GameObject.Instantiate(plane);
-            renderer = visualizer[i].GetComponent<Renderer>();
-            renderer.material.color = (i%2 == 0 ) ? Color.blue : Color.green;
-            visualizer[i].transform.parent = this.transform;
+            visualizer[i] = Instantiate(plane);
+            //renderer = visualizer[i].GetComponent<Renderer>();
+            //renderer.material.color = (i%2 == 0 ) ? Color.blue : Color.green;
+            visualizer[i].transform.parent = NewSP_handle;
             zscale = visualizer[i].transform.localScale.z;
-            visualizer[i].transform.localPosition = /*this.transform.position + */new Vector3(0, -0.001f, -0.03f * i) + displacement;
+            visualizer[i].transform.localPosition = /*this.transform.position + */new Vector3(0, -0.051f, -1.0f * i);
             visualizer[i].transform.localScale = new Vector3(0, 0, 0);
         }
     }
 	
+    void condense()
+    {
+        uint diff = fullLength / length;
+        int j = 0;
+        for (int i = 0; i < length; i++)
+        {
+            j = 0;
+            spectrumVal[i] = 0;
+            while (j < diff)
+            {
+                spectrumVal[i] += spectrum[i * diff + (j++)];
+            }
+
+        }
+
+    }
+
 	// Update is called once per frame
 	void Update () {
-        AudioListener.GetSpectrumData(spectrum, 0, window);
+
+        float multiplier = 1.0f;
+        //This is temporary : change when convenient
+        if (Math.Abs(Player.transform.position.x - NewSP_handle.position.x) < accuracy)
+        {
+            multiplier = 1.0f;
+            AudioListener.GetSpectrumData(spectrum, 0, window);
+            condense();
+        }
+        else
+            multiplier = 0.0f;
+
         for(int i=0;i< length; i++)
         {
-            visualizer[i].transform.localScale = new Vector3(spectrum[i] * 10.0f, 0.5f, 0.003f);
+            visualizer[i].transform.localScale = new Vector3(spectrumVal[i% fftPrecision] * 5.0f, 0.05f, 0.1f)*multiplier;
         }
 
     }
